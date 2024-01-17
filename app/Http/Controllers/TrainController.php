8ba6dccbc2d4fed32354c0e58f12bee0c02e825f
DarioMaza <dmaza1907@gmail.com>
2024-01-17 14:48:46 +0100

    17-01-2024

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
@@ -11,6 +12,7 @@ class TrainController extends Controller
      */
     public function index()
     {
+        $trains = Train::all();
         return view()
     }
 
